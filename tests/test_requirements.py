e8624bec2857319aad3e2e768cb0bc00eee151e6
Karol Nowacki <karol@b24net.pl>
2025-04-15 11:59:53 +0200

    Fix tests

--- tests/test_requirements.py
+++
@@ -1,9 +0,0 @@
-
-from ..colonization.requirements import requirements
-
-def test_types():
-    assert type(requirements.types()) is list
-
-def test_get():
-    assert requirements.get(requirements.types()[10])['needed']['steel'] > 0
-    assert requirements.get(None) == {}
